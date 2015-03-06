07a401ce1e5f771746f66bd1f7a0c7ba33788381
Kristen MacFarlane <krismacfarlane@gmail.com>
2015-03-06 11:40:21 -0500

    adds new map views with js and css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,43 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+
+  L.mapbox.accessToken = 'pk.eyJ1Ijoia3Jpc21hY2ZhcmxhbmUiLCJhIjoiN3lkS25jcyJ9.6chNwLDVAAfQacy0JKvNxQ';
+  var map = L.mapbox.map('map', 'examples.map-i86nkdio')
+      .setView([40.748817, -73.985428], 17);
+
+  var featureGroup = L.featureGroup().addTo(map);
+
+  var makeSidebar = function() {
+    console.log("fired");
+    $('#popup').removeClass('hidden');
+  }
+
+  var drawControl = new L.Control.Draw({
+    edit: {
+      featureGroup: featureGroup
+    }
+  }).addTo(map);
+
+  map.on('draw:created', function(e) {
+    var type = e.layerType,
+     layer = e.layer;
+
+    if (type === 'marker') {
+      makeSidebar();
+      var form = $("form#popup");
+      form.on("submit", function(e){
+        e.preventDefault();
+        var popupContent = $("#popup-box").val();
+        layer.bindPopup(popupContent);
+        $('#popup').addClass('hidden');
+      });
+    }
+
+   featureGroup.addLayer(layer);
+   console.log(featureGroup.getBounds(layer));
+  });
+
+});
--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,3 +1,11 @@
 class MapsController < ApplicationController
 
+  def index
+
+  end
+
+  def new
+    @map = Map.new
+  end
+
 end
