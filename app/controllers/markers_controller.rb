3e6ad2e52211a0760242abddaa424cb722d835c1
Kristen MacFarlane <krismacfarlane@gmail.com>
2015-03-07 20:07:34 -0500

    trying to fix ajax error

--- app/controllers/markers_controller.rb
+++ app/controllers/markers_controller.rb
@@ -1,7 +1,11 @@
 class MarkersController < ApplicationController
 
+  def index
+  end
+
   def create
-    @map = Map.find(params[:map_id])
+    @map = Map.find(params[:id])
+    binding.pry
     @map.markers << Marker.new(lat: params[:lat].to_f, long: params[:long].to_f, popup_content: params[:popup_content])
   end
 
