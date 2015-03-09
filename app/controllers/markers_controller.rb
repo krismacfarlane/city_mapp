0e142e0c9af576d4af443c9f9790cbfe117e83bf
Kristen MacFarlane <krismacfarlane@gmail.com>
2015-03-09 15:39:27 -0400

    fixes the ajax response error

--- app/controllers/markers_controller.rb
+++ app/controllers/markers_controller.rb
@@ -8,6 +8,7 @@ class MarkersController < ApplicationController
   def create
     @map = Map.find(params[:map_id])
     @map.markers << Marker.new(lat: params[:lat].to_f, long: params[:long].to_f, popup_content: params[:popup_content])
+    render json: @map
   end
 
 end
