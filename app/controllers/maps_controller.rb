9541d3879d0a03c5077955e88b45159f89fe678c
Philip Hughes <philip@h4w5.com>
2015-03-09 10:36:47 -0400

    refactor maps controller as an example

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,34 +1,30 @@
 class MapsController < ApplicationController
+  before_action :load_city
 
   def index
-    @maps = Map.where(city_id: params[:city_id])
-    @city = @maps.first.city.name
+    @maps = @city.maps
   end
 
   def new
-    @city = City.find(params[:city_id])
     @map = Map.new(:city=>@city)
   end
 
   def create
-    @city = params[:city_id]
-    @map = Map.new
-    @map.title = params[:map][:title]
-    @map.city_id = @city
-    @map.user_id = session[:user_id]
-    @map.save
+    @map  = @city.maps.create(
+      title: params[:map][:title],
+      user:  current_user
+    )
     redirect_to city_map_path(@city, @map)
   end
 
   def show
-    @city = City.find(params[:city_id])
-    @map = Map.find(params[:id])
+    @map  = Map.find(params[:id])
   end
 
   private
 
-  def map_params
-    params.require(:map).permit(:title)
+  def load_city
+    @city = City.find(params[:city_id])
   end
 
 end
